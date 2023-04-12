d04fdad378d394ae47cdc9c81bd8c21b6e125e3d
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-04-12 08:19:46 +0700

    added support for safari & chrome

--- components/navside/Navside.tsx
+++ components/navside/Navside.tsx
@@ -1,6 +1,6 @@
 import styles               from './styles/styles.module.scss'
 
-import {default as React, useState} from 'react'
+import {default as React, useState, useRef, useEffect} from 'react'
 
 // internals:
 import {
@@ -28,6 +28,11 @@ export interface NavsideProps<TElement extends Element = HTMLElement>
     extends
         ThemableProps
 {
+    // animations:
+    transitionInterval  ?: number
+    
+    
+    
     // children:
     children            ?: React.ReactNode
 }
@@ -39,6 +44,11 @@ export const Navside = <TElement extends Element = HTMLElement>(props: NavsidePr
     
     // rest props:
     const {
+        // animations:
+        transitionInterval = 300, // ms
+        
+        
+        
         // children:
         children,
     } = props;
@@ -56,12 +66,89 @@ export const Navside = <TElement extends Element = HTMLElement>(props: NavsidePr
     
     
     
+    // transitions:
+    const navsideRef = useRef<HTMLElement>(null);
+    const prevSelectedIndex = useRef<number>(menuSelectedIndex);
+    const firstPositioning = useRef<boolean>(true);
+    const menuSelectedIndexProp = NavsidePublicVars.MenuSelectedIndex;
+    useEffect(() => {
+        // conditions:
+        const navsideElm = navsideRef.current as Element as HTMLElement;
+        if (!navsideElm) return;
+        if (prevSelectedIndex.current === menuSelectedIndex) return;
+        
+        
+        
+        if (firstPositioning.current) {
+            firstPositioning.current = false;
+            prevSelectedIndex.current = menuSelectedIndex;
+            navsideElm.style.setProperty(menuSelectedIndexProp, `${prevSelectedIndex.current}`);
+        } // if
+        
+        
+        
+        // handlers:
+        const transitionSpeed = (menuSelectedIndex - prevSelectedIndex.current) / transitionInterval;
+        let previousTime : number|undefined = undefined;
+        const handleAnimate : FrameRequestCallback = (currentTime) => {
+            if (previousTime === undefined) {
+                previousTime = currentTime;
+                cancelAnimating = requestAnimationFrame(handleAnimate);
+                return;
+            } // if
+            const deltaTime = currentTime - previousTime;
+            previousTime = currentTime;
+            
+            const remainingTransition = menuSelectedIndex - prevSelectedIndex.current;
+            let deltaTransition = transitionSpeed * deltaTime;
+            if (remainingTransition >= 0) {
+                deltaTransition = Math.min(Math.max(0, deltaTransition), remainingTransition);
+            }
+            else {
+                deltaTransition = Math.max(Math.min(0, deltaTransition), remainingTransition)
+            } // if
+            prevSelectedIndex.current += deltaTransition;
+            
+            
+            
+            if (Math.abs(menuSelectedIndex - prevSelectedIndex.current) > 0.01) {
+                cancelAnimating = requestAnimationFrame(handleAnimate);
+            }
+            else {
+                prevSelectedIndex.current = menuSelectedIndex;
+            } // if
+            
+            
+            
+            navsideElm.style.setProperty(menuSelectedIndexProp, `${prevSelectedIndex.current}`);
+        };
+        
+        
+        
+        // setups:
+        let cancelAnimating = requestAnimationFrame(handleAnimate);
+        
+        
+        
+        // cleanups:
+        return () => {
+            cancelAnimationFrame(cancelAnimating);
+        };
+    }, [menuSelectedIndex, menuSelectedIndexProp, transitionInterval]);
+    
+    
+    
     // jsx:
     return (
         <nav
+            // refs:
+            ref={navsideRef}
+            
+            
+            
             // styles:
             style={{
-                [NavsidePublicVars.MenuSelectedIndex as any]: menuSelectedIndex,
+                [NavsidePublicVars.MenuSelectedIndex as any]: prevSelectedIndex.current,
             }}
             
             
